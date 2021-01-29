5293b35063b77d5ad7fbe94345c5d1b79806c20c
Jonjo Jefferson <jonjo@amicistudios.com>
2021-01-29 20:31:10 +0000

    updated imports for better CI

--- src/app/offices/offices.component.ts
+++ src/app/offices/offices.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-offices',
   templateUrl: './offices.component.html',
-  styleUrls: ['./offices.component.scss']
+  styleUrls: ['./offices.component.scss'],
 })
 export class OfficesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
