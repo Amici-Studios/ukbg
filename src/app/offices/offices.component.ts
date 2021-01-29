080368ae60b8a02572691d16f94d4018c5265b04
Jonjo Jefferson <jonjo@amicistudios.com>
2021-01-29 23:31:28 +0000

    fixed

--- src/app/offices/offices.component.ts
+++ src/app/offices/offices.component.ts
@@ -1,6 +1,8 @@
+import { FormsModule } from '@angular/forms';
+import { GeneralApiService } from './../shared/services/general-api.service';
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { FirestoreService } from '../shared/services/firestore.service';
-import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-offices',
@@ -8,18 +10,35 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./offices.component.scss'],
 })
 export class OfficesComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private firestore: FirestoreService) {}
-  officeInfo: any;
-  countryCollectionId = 'England';
-  countyDocId = 'Bedfordshire';
-  officeCollectionId = 'Bedfordshire Office';
-  ngOnInit(): void {
-    this.getRegisterOffice();
-  }
+  constructor(private fireStoreService: FirestoreService, private general: GeneralApiService) {}
+  countryCollectionId = '';
+  countyDocId = '';
+  officeCollectionId = '';
+  offices: any;
+  charities: any;
+  office: any;
+  county: any;
+  bedfordshire = [{ countryCollectionId: 'England', countyDocId: 'Bedfordshire', officeCollectionId: 'Bedfordshire Office' }];
+
+  ngOnInit() {}
 
   getRegisterOffice() {
-    this.firestore.getOffices(this.countryCollectionId, this.countyDocId, this.officeCollectionId).subscribe((res) => {
-      this.officeInfo = res;
+    this.fireStoreService.getOffices(this.countryCollectionId, this.countyDocId, this.officeCollectionId).subscribe((res) => {
+      this.offices = res;
+      console.log(res);
+    });
+  }
+
+  getLancashireRegisterOffice() {
+    this.fireStoreService.getLancashire().subscribe((res) => {
+      this.offices = res;
+      console.log(this.offices);
+    });
+  }
+
+  getCharities() {
+    this.general.getCharities().subscribe((res) => {
+      this.charities = res;
       console.log(res);
     });
   }
